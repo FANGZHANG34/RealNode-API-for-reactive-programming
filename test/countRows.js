e311e32a5e49bf54cfcdc85bae6db0844d1ff43d
方丈34 <168342791+FANGZHANG34@users.noreply.github.com>
2025-02-07 16:57:48 +0800

    Update countRows.js

--- test/countRows.js
+++ test/countRows.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 ['../index.js'].
 reduce((s,subPath)=>fs.readFile(path.resolve(__dirname,subPath),'utf-8',(e,value)=>{
-    const testReg = /^[\s\t]{0,}[/\*]/;
-    console.log(
-        subPath,e ?? value.split(/\n|\r/).
-        reduce((c,str)=>(str && (testReg.test(str) ? c[1]++ : c[0]++),c),[0,0]).
-        reduce((s,v,i,arr)=>i === 1 ? [...arr,arr[0] / (arr[0] + arr[1])] : s)
-    );
+	const testReg = /^[\s\t]{0,}[/\*]/;
+	console.log(
+		subPath,e ?? value.split(/\n|\r/).
+		reduce((c,str)=>(str && (testReg.test(str) ? c[1]++ : c[0]++),c),[0,0]).
+		reduce((s,v,i,arr)=>i === 1 ? [...arr,arr[0] / (arr[0] + arr[1])] : s)
+	);
 }),0);
