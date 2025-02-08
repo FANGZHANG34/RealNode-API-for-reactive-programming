6eb378f8c4180053d9669fb1d2e16ee4bd4c9db8
方丈34 <168342791+FANGZHANG34@users.noreply.github.com>
2025-02-08 15:11:10 +0800

    Update tryRealStory.js

--- test/tryRealStory.js
+++ test/tryRealStory.js
@@ -1,5 +1,11 @@
 "use strict";
-const {RealWorld,RealNode,RealElement,RealGroup,RealStory,RealPromise} = require('../');
+const getPath = (()=>{
+    const path = require('path');
+    return path.resolve.bind(path,__dirname);
+})();
+// require(getPath('./countRows.js'));
+const {RealWorld,RealNode,RealElement,RealGroup,RealStory,RealPromise} = require(getPath('../'));
+const process = require('process');
 
 RealStory.newPrivatePage(async page=>console.log(await page.then(()=>new RealElement({self:{}}))));
 RealNode.time((temp=>{
@@ -10,9 +16,10 @@ RealNode.time((temp=>{
 	;
 	teacher.react = ()=>{
 		const queue = boy > girl ? [boy,girl] : [girl,boy];
-		queue[0] > 1e6 ? temp.resolve(queue[0]+' : '+(queue[0] - queue[1])) : Math.random() > Math.random() ? boy.value++ : girl.value++;
+		queue[0] < 1e6 ? Math.random() > Math.random() ? boy.value++ : girl.value++ : temp.resolve(queue[0]+' : '+(queue[0] - queue[1]));
 	};
 	teacher.react();
 	return temp.self;
 })(RealStory.newPage().newPromiseObj())).then(result=>console.log('\n',result.value,'in +',result.time,'ms\n'));
-RealStory.then(()=>console.log(new RealElement({self:{}})+'')).newPage().then(async()=>console.log(await RealNode.afterNow(()=>'成功承诺化'))).then(()=>process.exit(0));
+RealStory.then(()=>console.log(new RealElement({self:{}})+'')).newPage().then(async()=>console.log(await RealNode.afterNow(()=>'成功承诺化')))
+.then(()=>process.exit(0));
