f9cefac8891ca6f614e921eb673d3392f906f26a
方丈34 <168342791+FANGZHANG34@users.noreply.github.com>
2025-03-23 21:09:09 +0800

    Add files via upload

--- test/countRows.js
+++ test/countRows.js
@@ -6,9 +6,9 @@ const getPath = path.resolve.bind(path,__dirname);
 
 ['../index.js'].
 reduce((s,subPath)=>fs.readFile(getPath(subPath),'utf-8',(e,value)=>{
-	const testReg = /^[\s\t]{0,}[/\*]/;
+	const testReg = /^[\s\t]*[\/\*]/;
 	console.log(
-		subPath,e ?? value.split(/\n|\r/).
+		subPath,e ?? value.split(/[\n\r]/).
 		reduce((c,str)=>(str && (testReg.test(str) ? c[1]++ : c[0]++),c),[0,0]).
 		reduce((s,v,i,arr)=>i === 1 ? [...arr,arr[0] / (arr[0] + arr[1])] : s)
 	);
--- test/tryRealStory.js
+++ test/tryRealStory.js
@@ -1,10 +1,10 @@
-"use strict";
-import process from "process";
-try{
-	const temp = ({default: exports})=>exports;
-	var require = require ?? (path=>import(String(path)).then(temp));
-}catch(e){console.error(e);}
-const {RealWorld,RealNode,RealElement,RealGroup,RealStory} = await require('../index.js');
+// "use strict";
+// import process from "process";
+// try{
+// 	const temp = ({default: exports})=>exports;
+// 	var require = require ?? (path=>import(String(path)).then(temp));
+// }catch(e){console.error(e);}
+// const {RealWorld,RealNode,RealElement,RealGroup,RealStory} = await require('../index.js');
 
 RealStory.newPrivatePage(async page=>console.log(await page.then(()=>new RealElement({self:{}}))));
 RealNode.time((()=>{
@@ -21,7 +21,7 @@ RealNode.time((()=>{
 		queue[0] < 512 ? tempFn() : temp.resolve('\t=>'+queue[0]+' : '+(queue[0] - queue[1]),teacher.value = queue[0].id.description+' wins!!!');
 	};
 	teacher.value = 'pending...';
-	return temp.self;
+	return temp.promise;
 })()).then(result=>console.log(result.value,'in +',result.time,'ms'));
 RealStory.then(()=>console.log(new RealElement({self:{}})+'')).newPage().then(async()=>console.log(await RealNode.afterNow(()=>'成功承诺化')))
 .then(()=>process.exit(0));
